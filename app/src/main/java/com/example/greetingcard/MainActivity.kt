b1916b540176183a2825022ff62a5de4dbc593a8
criptowaco <criptowaco@gmail.com>
2025-08-28 13:51:41 +0200

    feat(ui): adopt codelab-style layout; keep cyan background; easy toggle between birthday/christmas

--- app/src/main/java/com/example/greetingcard/MainActivity.kt
+++ app/src/main/java/com/example/greetingcard/MainActivity.kt
@@ -3,56 +3,87 @@ package com.example.greetingcard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.greetingcard.ui.theme.GreetingCardTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            // Change this line to display the desired greeting
-            //GreetingScreen(message = christmasGreeting("Hugo"))
-            GreetingScreen(message = birthdayGreeting("Hugo", 3))
+            GreetingCardTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = Color.Cyan
+                ) {
+                    // Switch between birthday or christmas here
+                    GreetingText(
+                        message = birthdayGreeting("Hugo", age = 3),
+                        from = "From Dad",
+                        modifier = Modifier.padding(8.dp)
+                    )
+                    // GreetingText(message = christmasGreeting("Hugo"), from = "From Dad")
+                }
+            }
+
         }
     }
 }
 
-@Composable
-fun GreetingScreen(message: String) {
-    GreetingCardTheme {
-        Surface(
-            modifier = Modifier.fillMaxSize(),
-            color = Color.Cyan
-        ) {
-            Text(
-                text = message,
-                modifier = Modifier.padding(24.dp)
-            )
-        }
-    }
-}
-
-// Returns a Christmas greeting
+// Custom messages
 fun christmasGreeting(name: String): String {
     return "Merry Christmas, $name!\n...and Happy New Year!"
 }
 
-// Returns a Birthday greeting
 fun birthdayGreeting(name: String, age: Int): String {
     return "Happy Birthday, $name!\nYou are now $age years old!"
+}
+
+// UI composable
+@Composable
+fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.fillMaxSize()
+    ) {
+        Text(
+            text = message,
+            fontSize = 32.sp,
+            lineHeight = 40.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = from,
+            fontSize = 20.sp,
+            modifier = Modifier
+                .padding(top = 16.dp, end = 16.dp)
+                .align(alignment = Alignment.End)
+        )
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    // GreetingScreen(message = christmasGreeting("Preview User"))
-    GreetingScreen(message = birthdayGreeting("Preview User", 3))
+    GreetingCardTheme {
+        GreetingText(
+            message = birthdayGreeting("Hugo", 3),
+            from = "From Dad"
+        )
+    }
 }
